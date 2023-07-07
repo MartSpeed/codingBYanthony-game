0d22cdcbc0be589760252471ef1c897c8b5c2488
John Anthony <jmanthony.fullsail@gmail.com>
2023-07-07 18:04:10 -0500

    CHORE: created variables for the x and y coordinates frames for player animation

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = (6876/12)
 const spriteHeight = (5230/10)
+let frameX = 0;
+let frameY = 0;
 
 //
 function animate() {
@@ -34,7 +36,7 @@ function animate() {
   // the second 4 determine the destination where to draw the cropped out part to
   // source image, source-image x-coordinates, source-image y-coordinates, source-image width, source-image height, destination x-coordinates, destination y-coordinates, destination width, destination height
   // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh)
-  ctx.drawImage(playerImage, 3 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   requestAnimationFrame(animate);
 }
 animate();
