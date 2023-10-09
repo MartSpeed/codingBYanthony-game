5191a19b6938cd6d8018364b5f08ba0acf627c95
John Anthony <jmanthony.fullsail@gmail.com>
2023-10-09 17:03:51 -0500

    chore: commiting my unfinished changes to come back to this later

--- playerBackground/script.js
+++ playerBackground/script.js
@@ -1,31 +1,30 @@
 //
-const canvas = document.getElementById('canvas1')
+const canvas = document.getElementById('canvas1');
 //
 const ctx = canvas.getContext('2d');
 //
-const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_WIDTH = (canvas.width = 800);
 //
-const CANVAS_HEIGHT = canvas.height = 700;
+const CANVAS_HEIGHT = (canvas.height = 700);
 // scrolling speed controller
 let gameSpeed = 5;
 // bring image into the canvas object
-const backgroundLayer1 = new Image()
-backgroundLayer1.src = 'layer-1.png'
-const backgroundLayer2 = new Image()
-backgroundLayer2.src = 'layer-2.png'
-const backgroundLayer3 = new Image()
-backgroundLayer3.src = 'layer-3.png'
-const backgroundLayer4 = new Image()
-backgroundLayer4.src = 'layer-4.png'
-const backgroundLayer5 = new Image()
-backgroundLayer5.src = 'layer-5.png'
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'layer-5.png';
 
 //
-function animate()
-{
-    ctx.drawImage(backgroundLayer5, 0, 0);
-    requestAnimationFrame(animate)
+function animate() {
+  ctx.drawImage(backgroundLayer5, 0, 0);
+  requestAnimationFrame(animate);
 }
-animate()
+animate();
 
 // 51:18 is where you stopped on the video
