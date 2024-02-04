45adc86c0a49147bdc3800b2f368209efae16f3f
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-03 19:17:44 -0600

    feat: background images now load in one after each other based on the current frame rate created. The game between images is expected right now, the point is to get the images to stream through all images seamlessley and then fix the following frame issues between each image

--- playerBackground/script.js
+++ playerBackground/script.js
@@ -22,14 +22,19 @@ backgroundLayer5.src = 'layer-5.png';
 
 // setting background scroll speed
 let x = 0;
+let x2 = 2400;
 
 function animate() {
   //
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.drawImage(backgroundLayer4, x, 0);
+  ctx.drawImage(backgroundLayer4, x2, 0);
   if (x < -2400) {
     x = 2400;
   } else x -= gameSpeed;
+  if (x2 < -2400) {
+    x2 = 2400;
+  } else x2 -= gameSpeed;
   requestAnimationFrame(animate);
 }
 animate();
