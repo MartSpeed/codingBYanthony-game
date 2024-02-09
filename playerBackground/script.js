5027b2188f82511e0a96a9bda11033659263d03e
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-08 19:58:34 -0600

    feat: all 5 layers work correctly and work with individual game speeds as well

--- playerBackground/script.js
+++ playerBackground/script.js
@@ -53,15 +53,27 @@ class Layer {
   }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.5);
+const layer2 = new Layer(backgroundLayer2, 0.5);
+const layer3 = new Layer(backgroundLayer3, 0.5);
 const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer5 = new Layer(backgroundLayer5, 0.5);
 
 function animate() {
   //
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // update layer
+  layer1.update();
+  layer2.update();
+  layer3.update();
   layer4.update();
+  layer5.update();
   // draw layer
+  layer1.draw();
+  layer2.draw();
+  layer3.draw();
   layer4.draw();
+  layer5.draw();
   requestAnimationFrame(animate);
 }
 animate();
