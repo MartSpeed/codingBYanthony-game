bbd02c0da02001411edd0f40d106e8ca239f752e
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-11 22:51:52 -0600

    feat: added the window load so that it can load within any browser being used

--- playerBackground/script.js
+++ playerBackground/script.js
@@ -23,67 +23,70 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-//slider
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
+//
+window.addEventListener('load', function () {
+  //slider
+  const slider = document.getElementById('slider');
+  slider.value = gameSpeed;
+  const showGameSpeed = document.getElementById('showGameSpeed');
+  showGameSpeed.innerHTML = gameSpeed;
 
-// the slider is not updating target value
-slider.addEventListener('change', function (e) {
-  this.gameSpeed = e.target.value;
-  showGameSpeed.innerHTML = e.target.value;
-});
-
-// creating a class to make the layers for the level
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-  // move layers horizontally by changing their x and y properties
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    if (this.x <= -this.width) {
-      this.x = 0;
-    }
-    this.x = this.x - this.speed;
-    // this.x = (gameFrame * this.speed) % this.width;
-  }
-  // draw the images to my canvas object selector
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(
-      this.image,
-      this.x + this.width,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-// creating an array to contain the layers to be called
-const gameObject = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  gameObject.forEach((object) => {
-    object.update();
-    object.draw();
+  // the slider is not updating target value
+  slider.addEventListener('change', function (e) {
+    this.gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
   });
-  // gameFrame--;
-  requestAnimationFrame(animate);
-}
-animate();
+
+  // creating a class to make the layers for the level
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+    // move layers horizontally by changing their x and y properties
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = this.x - this.speed;
+      // this.x = (gameFrame * this.speed) % this.width;
+    }
+    // draw the images to my canvas object selector
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
+
+  // creating an array to contain the layers to be called
+  const gameObject = [layer1, layer2, layer3, layer4, layer5];
+
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObject.forEach((object) => {
+      object.update();
+      object.draw();
+    });
+    // gameFrame--;
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
