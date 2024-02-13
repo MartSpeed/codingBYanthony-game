4b090265ea7d25fb516fa769133ef2f902513bea
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-13 06:58:37 -0600

    feature: manual animation method created

--- enemyObject/script.js
+++ enemyObject/script.js
@@ -7,9 +7,18 @@ const CANVAS_HEIGHT = (canvas.height = 1000);
 
 // enemy factory
 enemy1 = {
-  x: 0,
-  y: 0,
+  x: 10,
+  y: 50,
   width: 200,
   height: 200,
 };
 
+// animation loop
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.x++;
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  requestAnimationFrame();
+}
+
+animate();
