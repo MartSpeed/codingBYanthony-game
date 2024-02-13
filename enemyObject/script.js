fd30f7dbe92c2a08c40bde430515758d76c1bb5b
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-13 06:53:21 -0600

    feat: enemy factory creation, what properties does the enemy need

--- enemyObject/script.js
+++ enemyObject/script.js
@@ -4,3 +4,12 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
+
+// enemy factory
+enemy1 = {
+  x: 0,
+  y: 0,
+  width: 200,
+  height: 200,
+};
+
