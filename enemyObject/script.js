fad94c3928a1c9c402c93bcf899d9712cd4d73af
John Anthony <jmanthony.fullsail@gmail.com>
2024-02-13 07:10:09 -0600

    feat: creating enemy instantiation

--- enemyObject/script.js
+++ enemyObject/script.js
@@ -6,17 +6,23 @@ const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
 
 // enemy factory
-enemy1 = {
-  x: 10,
-  y: 50,
-  width: 200,
-  height: 200,
-};
+class Enemy {
+  constructor(x, y, width, height) {
+    this.x = 10;
+    this.y = 50;
+    this.width = 36;
+    this.height = 36;
+  }
+}
+
+// instantiate
+const enemy1 = new Enemy();
 
 // animation loop
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   enemy1.x++;
+  enemy1.y++;
   ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
   requestAnimationFrame();
 }
